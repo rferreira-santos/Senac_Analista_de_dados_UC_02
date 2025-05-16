2d3bb9c2614314678e9065b4720ef06aa3a018c4
rferreira-santos <rodrigoferreirasantos@yahoo.com.br>
2025-05-15 21:33:16 -0300

    Add files via upload

--- Aula_04/Aula_04_01.py
+++ Aula_04/Aula_04_01.py
@@ -23,6 +23,9 @@ media_salario = np.mean(array_salario)
 maior_salario = np.max(array_salario)
 menor_salario = np.min(array_salario)
 nome_salario = df_funcionarios[df_funcionarios['Salário'] == maior_salario]['Nome']
+amplitude_salario = maior_salario - menor_salario
+mediana_salario = np.median(array_salario)
+distancia_salario = abs((media_salario - mediana_salario)/mediana_salario)
 
 media_idade = np.mean(array_idade)
 maior_idade = np.max(array_idade)
@@ -30,13 +33,16 @@ menor_idade = np.min(array_idade)
 amplitude_idade = maior_idade - menor_idade
 nome_velho = df_funcionarios[df_funcionarios['Idade'] == maior_idade]['Nome']
 nome_novo = df_funcionarios[df_funcionarios['Idade'] == menor_idade]['Nome']
+mediana_idade = np.median(array_idade)
+distancia_idade = abs((media_idade - mediana_idade)/mediana_idade)
 
 media_tempo = np.mean(array_tempo)
 maior_tempo = np.max(array_tempo)
 menor_tempo = np.min(array_tempo)
 amplitude_tempo = maior_tempo - menor_tempo
 nome_tempo = df_funcionarios[df_funcionarios['Tempo'] == maior_tempo]['Nome']
-
+mediana_tempo = np.median(array_tempo)
+distancia_tempo = abs((media_tempo - mediana_tempo)/ mediana_tempo)
 qtd_func = np.count_nonzero(array_idade)
 
 # Exibindo as Métricas
@@ -44,18 +50,26 @@ print('\n----------- Tabela de Funcionários -------------')
 print(df_funcionarios)
 print('\n-------- Exibindo os Resultados Solicitados --------')
 
-print(f'A média salarial dos funcionários é {media_salario:.2f}')
+print(f'A média salarial dos funcionários é R${media_salario:.2f} Reais')
 print(f'{nome_salario.values[0]} é o funcionário com maior salário.')
+print(f'A Amplitude relativa aos sálarios dos funcionários da empresa é {amplitude_salario} anos')
+print(f'A mdiana dos valores salarias é R${mediana_salario:.0f} Reais')
+print(f'A distância entre a média e a mediana dos salários dos funcionários é {distancia_salario}')
+
 
 print(f'\n{nome_velho.values[0]} é o funcionário com mais idade da empresa, com {maior_idade} anos')
 print(f'{nome_novo.values[0]} é o funcionário com menos idade da empresa, com {menor_idade} anos')
 print(f'A Amplitude relativa a idade dos funcionários da empresa é {amplitude_idade} anos')
-print(f'A média da idade dos funcionários é {media_idade:.0f} anos')
+print(f'A média das idades dos funcionários é {media_idade:.0f} anos')
+print(f'A mdiana das idades dos funcionários é {mediana_idade:.0f} anos')
+print(f'A distância entre a média e a mediana das idades dos funcionários é {distancia_idade}')
 
 print(f'\n{nome_tempo.values[0]} é o funcionário com mais tempo de empresa')
 print(f'O Funcionário com maior tempo de empresa possui {maior_tempo} anos')
 print(f'O Funcionário com menor tempo de empresa possui {menor_tempo} anos')
 print(f'A Amplitude relativa ao tempo de empresa é {amplitude_tempo} anos')
 print(f'O tempo médio dos funcionários na empresa é {media_tempo:.0f} anos')
+print(f'A mdiana do tempo de empresa dos funcionários é {mediana_tempo:.0f} anos')
+print(f'A distância entre a média e a mediana do tempo de empresa dos funcionários é {distancia_tempo}')
 
 print(f'\nA quantidade de funcionários na empresa são {qtd_func}')
---
+++ Aula_05/aula_05_01.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+
+print('---- OBTENDO DADOS ----')
+
+# Importando a base de dados financeira.csv
+endereco_dados = 'BASES\Titanic.csv'
+
+# Criando o DataFrame financeira
+df_titanic = pd.read_csv(endereco_dados,sep=';',encoding='iso-8859-1')
+df_titanic = df_titanic[['Name','Sex','Age','Fare','Survived']]
+
+# Exibindo a base de dados financeira
+print('\n---- EXIBINDO A BASE DE DADOS -----')
+print(df_titanic)
+
+# Criando o array do valor da passagem e da idade
+array_titanic_fare = np.array(df_titanic["Fare"])
+array_titanic_age = np.array(df_titanic["Age"])
+array_titanic_survived = np.array(df_titanic["Survived"])
+
+# Obtendo a média do valor da passagem e da idade
+media_fare = np.mean(array_titanic_fare)
+media_age = np.mean(array_titanic_age)
+
+# Obtendo a mediana do valor da passagem e da idade
+mediana_fare = np.median(array_titanic_fare)
+mediana_age = np.median(array_titanic_age)
+
+# Obtendo a distância entre a média e a mediana do valor da passagem e da idade
+distancia_fare = abs((media_fare - mediana_fare) / mediana_fare)
+distancia_age = abs((media_age - mediana_age) / mediana_age)
+
+# Obtendo o máximo e o mínimo do valor da passagem e da idade
+maximo_fare = np.max(array_titanic_fare)
+maximo_age = np.max(array_titanic_age)
+minimo_fare = np.min(array_titanic_fare)
+minimo_age = np.min(array_titanic_age)
+
+# Obtendo a amplitude do valor da passagem e da idade
+amplitude_fare = maximo_fare - minimo_fare
+amplitude_age = maximo_age - minimo_age
+
+# Obtendo a quantidade de sobreviventes
+qtd_passageiros = df_titanic['Survived'].count()
+qtd_sobreviventes = df_titanic[df_titanic['Survived'] == 1].count()
+percentual_sobreviventes = (qtd_sobreviventes / qtd_passageiros) * 100
+df_titanic_survived = df_titanic[df_titanic['Survived'] == 1]
+
+# Obtendo a quantidade total de homens e mulheres
+qtd_masc = df_titanic[df_titanic['Sex'] == 'male'].count()
+qtd_fem = df_titanic[df_titanic['Sex'] == 'female'].count()
+
+# Obtendo a quantidade de homens e mulheres que sobreviveram
+qtd_masc_sobre = df_titanic_survived[df_titanic_survived['Sex'] == 'male'].count()
+qtd_fem_sobre = df_titanic_survived[df_titanic_survived['Sex'] == 'female'].count()
+
+
+# Exibindo os dados sobre as idades dos passageiros
+print("\n-- OBTENDO INFORMAÇÕES SOBRE AS IDADES --")
+print('Medidas de Tendência Central')
+print(f"\nA média das idades é {media_age:.1f}")
+print(f"A mediana das idades é {mediana_age:.1f}")
+print(f"A distância entre a média e a mediana é {distancia_age}")
+print(f"O menor valor das idades é {minimo_age}")
+print(f"O maior valor das idades é {maximo_age}")
+print(f"A amplitude dos valores das idades é {amplitude_age}")
+
+
+# Exibindo os dados sobre o valor das passagens
+print("\n-- OBTENDO INFORMAÇÕES SOBRE AS PASSAGENS --")
+print('Medidas de Tendência Central')
+print(f"\nA média das passagens é {media_fare:.2f}")
+print(f"A mediana das passagens é {mediana_fare:.2f}")
+print(f"A distância entre a média e a mediana é {distancia_fare}")
+print(f"O menor valor das passagens é {minimo_fare:.2f}")
+print(f"O maior valor das passagens é {maximo_fare:.2f}")
+print(f"A amplitude dos valores das passagens é {amplitude_fare:.2f}")
+
+
+# Exibindo os dados sobre os sobreviventes
+print("\n-- OBTENDO INFORMAÇÕES SOBRE OS SOBREVIVENTES --")
+print(f"A quantidade de passageiros foi {qtd_passageiros}")
+print(f"A quantidade de sobreviventes foi {qtd_sobreviventes.values[0]}")
+print(f"O percentual de sobreviventes foi {percentual_sobreviventes.values[0]:.2f} %")
+print(f"A quantidade de homens sobreviventes foi {qtd_masc_sobre.values[0]}")
+print(f"A quantidade de mulheres sobreviventes foi {qtd_fem_sobre.values[0]}")
+
+
+# Exibindo os dados sobre os passageiros
+print("\n-- OBTENDO INFORMAÇÕES SOBRE OS PASSAGEIROS --")
+print(f"A quantidade total de homens é {qtd_masc.values[0]}")
+print(f"A quantidade total de mulheres é {qtd_fem.values[0]}")
+
+
+
+
+
+
